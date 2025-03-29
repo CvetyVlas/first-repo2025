c6605a320c73bffc8f04a5c1a0680aafe59b9229
CvetyVlas <stela_iva@abv.bg>
2025-03-29 13:08:34 +0200

    Create new problem

---
+++ first repo2025/CenturiesToMinutes/ExchangeValue/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ExchangeValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            var c = 0;
+
+            Console.WriteLine("Before");
+            Console.WriteLine("a= "+a);
+            Console.WriteLine("b= "+b);
+
+            c = a;
+            a = b;
+            b = c;
+
+            Console.WriteLine("After");
+            Console.WriteLine("a= "+a);
+            Console.WriteLine("b= "+b);
+
+        }
+    }
+}
---
+++ first repo2025/CenturiesToMinutes/HexadecimalConvert/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HexadecimalConvert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var a = int.Parse(Console.ReadLine());
+            string b = Convert.ToString(a, 16).ToUpper();
+            string c = Convert.ToString(a, 2).ToUpper();
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+
+            //string a = Console.ReadLine();
+            //int b = Convert.ToInt32(a, 16);
+            //int c = Convert.ToInt32(a, 2);
+            //Console.WriteLine(b);
+            //Console.WriteLine(c);
+        }
+    }
+}
